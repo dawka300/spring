9f4eb5f2b61369c138678f1c3938d49a1f8cbe93
dwesolowski <dawid.wesolowski@silkycoders.com>
2023-03-17 11:57:41 +0100

    init

---
+++ src/main/java/dawid/demo/DawidApplication.java
@@ -0,0 +1,13 @@
+package dawid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dawid/demo/DawidApplicationTests.java
@@ -0,0 +1,13 @@
+package dawid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
